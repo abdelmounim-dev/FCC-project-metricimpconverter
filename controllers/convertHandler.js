7d587c78a7d72e0b7a1970f97148cd232fbfba60
abdelmounim-dev <77006851+abdelmounim-dev@users.noreply.github.com>
2023-09-11 21:58:12 +0000

    the api works,  need to implement the unit and function tests

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,42 +1,65 @@
+const { units, conversionRate, unitMapping } = require("../utils/constants");
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
+    let invalid = -1;
+    const alpha = /[a-zA-Z]/
+    const index = input.split('').findIndex(char => alpha.test(char))
+
+    if (index === 0) return 1;
+
+    let quantity = index > 0 ? input.slice(0, index): input.slice(0)
+
+    if(Number(quantity) < 0) return invalid
+
+    let quantityArray = quantity.split('/')
+
+    if(quantityArray.length === 1) {
+      const q = quantityArray[0]
+      if(q === '') return invalid
+      return isNaN(+q) ? invalid : +q
+    }
+
+    if(quantityArray.length === 2) {
+      if( quantityArray.some(num => isNaN(+num) || num === '')) return invalid
+      return  quantityArray[0] / quantityArray[1]
+    }
+
     
-    return result;
+    return invalid;
   };
   
   this.getUnit = function(input) {
-    let result;
-    
-    return result;
+    let invalid = -1;
+    const alpha = /[a-zA-Z]/
+    const index = input.split('').findIndex(char => alpha.test(char))
+
+    if (index < 0) return invalid;
+    let unit = input.slice(index)
+    return this.spellOutUnit(unit)
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
     
-    return result;
+    return units[initUnit];
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+    if (unit === "L" || unit === "l") return "L";
+    if (units.hasOwnProperty(unit.toLowerCase())) {
+      return unit.toLowerCase();
+    }
+    return -1;
   };
   
   this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    let result =  initNum * conversionRate[initUnit]
+    return [Number(result.toFixed(5)), this.getReturnUnit(initUnit)]
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    return `${initNum} ${unitMapping[initUnit]} converts to ${returnNum} ${unitMapping[returnUnit]}`
+   
   };
   
 }
--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,28 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert').get((req, res) => {
+    let {input} = req.query
+
+    if(!input) return res.send('invalid input')
+    
+    const initNum= convertHandler.getNum(input)
+    const initUnit= convertHandler.getUnit(input) 
+
+    if (initNum === -1) {
+      if(initUnit === -1)
+        {return  res.send('invalid number and unit');}
+      else {return res.send('invalid number');}
+    }
+    if(initUnit === -1)
+    {return res.send('invalid unit');}
+
+    let [returnNum, returnUnit] = convertHandler.convert(initNum, initUnit)
+    let str = convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
+    res.send({
+      initNum, initUnit, returnNum, returnUnit, string: str
+    })
+
+  })
+
 };
